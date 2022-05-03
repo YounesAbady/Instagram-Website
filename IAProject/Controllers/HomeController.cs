a6b08eb4db9ebdd8b80c65b583466f76ccdd81e1
YounesAbady <102363875+YounesAbady@users.noreply.github.com>
2022-05-03 23:37:42 +0200

    Project Intial

---
+++ IAProject/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IAProject.Models;
+
+namespace IAProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        ProjectDataBase db = new ProjectDataBase();
+        public ActionResult Index()
+        {
+            List<user> users = db.users.ToList();
+            return View(users);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
