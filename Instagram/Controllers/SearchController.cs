2c335f36efc869f842706920a1d9f123155968d0
mookhaled <104018130+mookhaled@users.noreply.github.com>
2022-05-06 20:44:16 +0200

    AfterSearch

---
+++ Instagram/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Instagram.Models;
+
+namespace Instagram.Controllers
+{
+    public class SearchController : Controller
+    {
+        ProjectDataBaseEntities db = new ProjectDataBaseEntities();
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Search(string Searching)
+        {
+            return View(db.users.Where(x => x.Fname.StartsWith(Searching) || x.Lname.StartsWith(Searching) || Searching == null).ToList());
+        }
+    }
+}
